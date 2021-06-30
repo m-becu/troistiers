21620bdc6894af24af2a2080a270c7a5140e1cf8
Anorak <contact.mbm@protonmail.com>
2021-06-30 17:41:41 +0200

    Update indexView to use template

--- www/view/indexView.php
+++ www/view/indexView.php
@@ -1,33 +1,30 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil</title>
-</head>
-<body>
-    <?php
-    if ($errorMsg != "") {
-        echo("<div class=error><p>$errorMsg</p></div>");
-    }
-    ?>
-    <h1>Bonjour!</h1>
-    <fieldset>
-        <legend>Formulaire de connexion</legend>
-        <form method="POST" action="/?action=connect">
-            <label>
-                Login
-                <input type="text" name="login" id="login-input">
-            </label>
-            <br>
-            <label>
-                Password
-                <input type="password" name="passw" id="passw-input">
-            </label>
-            <br>
-            <input type="submit" value="Envoyer">
-        </form>
-    </fieldset>
-</body>
-</html>
+<?php $title = "Bienvenue"; ?>
+
+<?php ob_start(); ?>
+
+<?php
+if ($errorMsg != "") {
+    echo("<div class=error><p>$errorMsg</p></div>");
+}
+?>
+<h1>Bonjour!</h1>
+<fieldset>
+    <legend>Formulaire de connexion</legend>
+    <form method="POST" action="/?action=connect">
+        <label>
+            Login
+            <input type="text" name="login" id="login-input">
+        </label>
+        <br>
+        <label>
+            Password
+            <input type="password" name="passw" id="passw-input">
+        </label>
+        <br>
+        <input type="submit" value="Envoyer">
+    </form>
+</fieldset>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require_once("components/template.php"); ?>
